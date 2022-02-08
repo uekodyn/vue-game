46031568fd4cfdcb9a906940c47012998444cb6b
Janderson Santos <jansantosd@gmail.com>
2022-02-08 15:58:08 -0300

    implementing background

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app">
+  <div id="app" :style="backgroundGradient" 
+    @mousemove="redefinePositions" 
+    @mouseleave="resetPositions">
     <div id="nav" />
     <router-view/>
   </div>
@@ -7,8 +9,33 @@
 
 
 <script>
+import { bgColorFrom, bgColorTo } from './styles/variables.scss';
+
   export default {
     name: 'App',
+    data() {
+      return {
+        gradientX: '50%',
+        gradientY: '0%',
+      };
+    },
+    computed: {
+      backgroundGradient() {
+        return {
+          background: `radial-gradient(circle at ${this.gradientX} ${this.gradientY}, ${bgColorFrom}, ${bgColorTo}`,
+        };
+      },
+    },
+    methods: {
+      redefinePositions({clientX, clientY}){
+        this.gradientX = `${clientX}px`;
+        this.gradientY = `${clientY}px`;
+      },
+      resetPositions(){
+        this.gradientX = '50%'
+        this.gradientY = '0%'
+      }
+    },
   };
 </script>
 
@@ -16,7 +43,9 @@
 @import './styles/reset';
 
 #app {
-		font-family: 'Barlow Semi Condensed', Helvetica, Arial, sans-serif;
-
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  font-family: 'Barlow Semi Condensed', Helvetica, Arial, sans-serif;
 }
 </style>
